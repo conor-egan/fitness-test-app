0be14ea2ecc197175451be1753782aae0aa6a56d
JamieMac96 <jamiemac96@hotmail.com>
2017-06-09 15:52:22 +0100

    basic signup functionality added

--- main.py
+++ main.py
@@ -1,58 +1,138 @@
 from boto3 import resource
-from flask import json, request
+from flask import json, request, abort
 from flask import Flask
 from decimal import Decimal
+from user_helper import SignUpCredential, LoginCredential, User
+
 application = Flask(__name__)
 
 JSONEncoder_olddefault = json.JSONEncoder.default
+
+
 def JSONEncoder_newdefault(self, o):
     if isinstance(o, Decimal):
         return float(o)
     return JSONEncoder_olddefault(self, o)
+
+
 json.JSONEncoder.default = JSONEncoder_newdefault
 
+dynamodb = resource('dynamodb', region_name='eu-west-1')
+user_table = dynamodb.Table('User')
+program_table = dynamodb.Table('Programs')
 
-@application.route('/users', methods=['GET', 'POST', ' PUT', 'DELETE'])
-def all_user_data():
-    dynamodb = resource('dynamodb', region_name='eu-west-1')
-    table = dynamodb.Table('User')
+
+@application.route('/users', methods=['GET', 'POST'])
+def users():
     if request.method == 'GET':
-        response = table.scan()
-        item = response['Items']
-        return json.jsonify(item)
+        return users_get()
     elif request.method == 'POST':
-        table.put_item(
-            Item={
-                'Username': request.form['Username'],
-                'email': request.form['email']
-            })
-
-        response = table.scan()
-        item = response['Items']
-        return json.jsonify(item)
+        return user_login()
 
 
-
-
-@application.route('/users/<string:Username>', methods=['GET', 'POST', ' PUT', 'DELETE'])
-def single_user_data(Username):
+@application.route('/users/<string:Username>', methods=['GET', 'PUT', 'DELETE'])
+def user(Username):
     if request.method == 'GET':
-        dynamodb = resource('dynamodb')
-        table = dynamodb.Table('User')
-        response = table.get_item(
-            Key={
-                'Username': Username
-            }
-        )
+        return user_get(Username)
+    elif request.method == 'PUT':
+        return create_user(Username)
+    elif request.method == 'DELETE':
+        return delete_user(Username)
+
+@application.route('/users/<string:Username>/programs', methods=['GET'])
+def user_programs(Username):
+    # This would probably query a separate table to the User table and
+    # return more detailed results
+    return json.jsonify(get_user_programs(Username))
+
+
+@application.route('/users/<string:Username>/programs/<string:program_name>', methods=['GET'])
+def user_program_details(Username, program_name):
+    # This would probably query a separate table to the User table and
+    # return more detailed results
+    return get_user_program(Username, program_name)
+
+# below are the helper methods
+
+
+def users_get():
+    response = user_table.scan()
+    item = response['Items']
+    users = list()
+    for user in item:
+        user_obj = User(user['Username'], user['fullname'], user['email'], user['liftmaxes'], user['programs'])
+        users.append(user_obj.to_dict())
+
+    return json.jsonify(users)
+
+
+def user_login():
+    credential = LoginCredential(request.form['Username'], request.form['password'])
+    user = user_table.get_item(
+        Key={
+            'Username': credential.username
+        }
+    )
+    item = user['Item']
+
+    if item['password'] == credential.password:
+        return json.jsonify({"success": True})
+    else:
+        return json.jsonify({"success": False})
+
+
+def user_get(Username):
+    response = get_user_response(Username)
+    if 'Item' not in response.keys():
+        return abort(404)
+    else:
         item = response['Item']
-
-        return json.jsonify(item)
-
-
+        user_obj = User(item['Username'], item['fullname'], item['email'], item['liftmaxes'], item['programs'])
+        return json.jsonify(user_obj.to_dict())
 
 
+def create_user(Username):
+    cred = SignUpCredential(Username, request.form['email'], request.form['fullname'], request.form['password'])
+    user_table.put_item(
+        Item={
+            'Username': cred.username,
+            'email': cred.email,
+            'fullname': cred.fullname,
+            'password': cred.password,
+            'liftmaxes': {},
+            'programs': [],
+            'prefferedunit': 'lbs'
+        })
 
+    response = get_user_response(Username)
+    item = response['Item']
+    return json.jsonify(item)
+
+
+def delete_user(Username):
+    user_table.delete_item(Key={
+        'Username': Username
+    })
+    return json.jsonify({"deleted": True})
+
+
+def get_user_programs(Username):
+    response = get_user_response(Username)
+    item = response['Item']
+    return item['programs']
+
+
+def get_user_program(Username, program_name):
+    return "implementation goes here"
+
+
+def get_user_response(Username):
+    response = user_table.get_item(
+        Key={
+            'Username': Username
+        }
+    )
+    return response
 
 if __name__ == "__main__":
     application.run('0.0.0.0')
-
---
+++ user_helper.py
@@ -0,0 +1,45 @@
+class SignUpCredential(object):
+    username = None
+    email = None
+    fullname = None
+    password = None
+
+    def __init__(self, username, email, fullname, password):
+        self.username = username
+        self.email = email
+        self.fullname = fullname
+        self.password = password
+
+
+class LoginCredential(object):
+
+    password = None
+    username = None
+
+    def __init__(self, username1, password1):
+        self.username = username1
+        self.password = password1
+
+
+
+class User(object):
+    username = None
+    fullname = None
+    email = None
+    liftmaxes = {}
+    programs = list()
+
+    def __init__(self, username, fullname, email,  liftmaxes, programs):
+        self.username = username
+        self.fullname = fullname
+        self.email = email
+        self.liftmaxes = liftmaxes
+        self.programs = programs
+
+
+    def to_dict(self):
+        user_dict = {'Username': self.username, 'fullname': self.fullname, 'email': self.email,
+                     'liftmaxes': self.liftmaxes, 'programs': self.programs}
+        return user_dict
+
+
